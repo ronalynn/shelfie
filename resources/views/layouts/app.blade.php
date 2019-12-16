e333e4198a29383fed80794cfe61a62855e17c9f
Lyn <ronalynnanong@live.com>
2019-12-16 20:38:35 +0000

    basic layout, folders for views, controllers added(empty)

---
+++ resources/views/layouts/app.blade.php
@@ -0,0 +1,14 @@
+<!DOCTYPE html>
+<html lang="en">
+  <head>
+    <title>Shelfie - @yield('title')</title>
+  </head>
+  <body>
+    <h1>Shelfie - @yield('title')</h1>
+
+    <div>
+      @yield('content')
+    </div>
+
+  </body>
+</html>
