ee28ec056a15e9e1cba602b3cb09a1a1f44f3ed6
Lyn <ronalynnanong@live.com>
2019-12-20 06:45:33 +0000

    one mo

--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -2,14 +2,14 @@
 
 @section('content')
 <div class="container">
-  <div class="row pt-3">
-    <ul>
-      @foreach($reviews as $review)
-        <li><a href="{{ route('reviews.show', ['review' => $review->id]) }}">{{$review->id}}</a></li>
-      @endforeach
+  <div class="card col-10 center">
+    <div class="card-header">
+      Welcome Back
+    </div>
+    <ul class="list-group list-group-flush">
+      <li class="list-group-item"><a href="profiles.index">Dashboard</a></li>
+      <li class="list-group-item"><a href="#">My Profile</a></li>
     </ul>
-
   </div>
-
 </div>
 @endsection
